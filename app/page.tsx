8c5b2abd10b9fd514ca5d5fe83fa963f0618cb4f
Ahmed Wael <awael3977@gmail.com>
2023-08-27 23:26:42 +0300

    Add all files again

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="flex flex-col items-center justify-between py-24">
+			<h1 className="text-3xl font-bold">Home page</h1>
+		</main>
+	);
+}
