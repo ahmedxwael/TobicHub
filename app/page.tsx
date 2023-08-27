84ad52ae5225eda0ee861aca045a74d65de223ab
Ahmed Wael <awael3977@gmail.com>
2023-08-27 23:25:08 +0300

    Move all the files

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<main className="flex flex-col items-center justify-between py-24">
-			<h1 className="text-3xl font-bold">Home page</h1>
-		</main>
-	);
-}
