c605429b2c85244f0fc879b580a4999c58626810
Ahmed Wael <awael3977@gmail.com>
2023-08-31 00:55:27 +0300

    Update error message of user-utils functions

--- utils/user-utils.ts
+++ utils/user-utils.ts
@@ -4,7 +4,7 @@ export const getUsers = async (): Promise<CreatorType[]> => {
 	const res = await fetch(`${process.env.BASE_URL}/api/users`);
 
 	if (!res.ok) {
-		throw new Error("Could not retrieve the list of topics.");
+		throw new Error("Could not retrieve the list of users.");
 	}
 
 	return res.json();
@@ -14,7 +14,7 @@ export const getUser = async (id: string): Promise<CreatorType> => {
 	const res = await fetch(`${process.env.BASE_URL}/api/users/${id}`);
 
 	if (!res.ok) {
-		throw new Error("Could not retrieve the list of topics.");
+		throw new Error("Could not retrieve the user.");
 	}
 
 	return res.json();
