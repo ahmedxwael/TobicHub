499ce7a020f5895e7345144d966aacd9c5b9f70d
Ahmed Wael <awael3977@gmail.com>
2023-09-01 09:08:10 +0300

    Make the page refreshes after reset

--- app/(topics)/error.tsx
+++ app/(topics)/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Error({
 	error,
@@ -9,11 +10,19 @@ export default function Error({
 	error: Error & { digest?: string };
 	reset: () => void;
 }) {
+	const router = useRouter();
+
 	return (
 		<section className="flex items-center justify-center gap-6 flex-col">
 			<h2>Something went wrong!</h2>
 			<div className="flex gap-4">
-				<button className="btn btn-alt" onClick={() => reset()}>
+				<button
+					className="btn btn-alt"
+					onClick={() => {
+						reset();
+						router.refresh();
+					}}
+				>
 					Try again
 				</button>
 				<Link href="/" className="btn btn-primary">
