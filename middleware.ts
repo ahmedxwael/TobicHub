eaa3bd75b5f9617108160c0a9fbea3764854e69c
Ahmed Wael <awael3977@gmail.com>
2023-09-17 14:19:14 +0300

    Update middleware approach

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,13 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export const middleware = async (req: NextRequest) => {
-	const pathname = req.nextUrl.pathname;
-	const token = await getToken({ req });
-
-	const isProtected =
-		pathname.startsWith("/create-topic") || pathname.startsWith("/edit-topic");
-
-	if (isProtected && !token) {
-		return NextResponse.redirect(new URL("/register", req.url));
-	}
-};
+export default withAuth({
+	pages: {
+		signIn: "/register",
+	},
+});
 
 export const config = {
 	matcher: [
-		"/",
-		"/topics",
 		"/create-topic",
 		"/edit-topic/:path*",
 		"/register",
