5a14a97f0dfd89d1e4ed4c486de3484bdf478d05
Ahmed Wael <awael3977@gmail.com>
2023-10-22 23:15:15 +0300

    Update getting topics number approach

--- components/user/posts-number.tsx
+++ components/user/posts-number.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import useGetUserTopics from "@/hooks/useGetTopics";
+import { TopicType } from "@/types";
+import { getUserTopics } from "@/utils/topic-utils";
+import { useEffect, useState } from "react";
 import { BiLoader } from "react-icons/bi";
 
 type PostsNumberProps = {
@@ -8,7 +11,23 @@ type PostsNumberProps = {
 };
 
 export default function PostsNumber({ userId }: PostsNumberProps) {
-	const { isLoading, topics } = useGetUserTopics(userId);
+	const [isLoading, setIsLoading] = useState(true);
+	const [topics, setTopics] = useState<TopicType[] | null>([]);
+
+	useEffect(() => {
+		(async function getTopics() {
+			setIsLoading(true);
+
+			try {
+				const topics = await getUserTopics(userId);
+				setTopics(topics);
+			} catch (error) {
+				throw new Error("Could not get user topics.");
+			}
+
+			setIsLoading(false);
+		})();
+	}, [userId]);
 
 	return (
 		<div className="text-neutral-400 flex items-center gap-2">
