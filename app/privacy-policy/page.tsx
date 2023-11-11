d279ff823d055c3b8cb9c3519ad9fdb02498c752
Ahmed Wael <awael3977@gmail.com>
2023-11-11 20:27:33 +0200

    Add privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,54 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <section className="w-full">
+      <div className="prose prose-headings:text-foreground prose-headings:mb-2 prose-p:text-muted-foreground w-screen max-w-full">
+        <h1>Privacy Policy for TopicHub</h1>
+        <h2>1. Introduction</h2>
+        <p>
+          Welcome to TopicHub. We are committed to protecting the privacy and
+          security of your personal information. This Privacy Policy outlines
+          the types of personal information we collect, how we use and protect
+          this information, and your rights concerning this information.
+        </p>
+        <h2>2. Information We Collect</h2>
+        <p>
+          Personal Information: We may collect personal information such as your
+          name, username and email address when you interact with our website or
+          services.
+        </p>
+        <h2>3. How We Use Your Information</h2>
+        <p>
+          We use your information for the following purposes: To communicate
+          with you regarding your posts state and updates.
+        </p>
+        <h2>4. Sharing Your Information</h2>
+        <p>
+          We do not sell, trade, or rent your personal information to third
+          parties.
+        </p>
+        <h2>5. Security</h2>
+        <p>
+          We take appropriate measures to protect your personal information.
+          However, no method of transmission over the internet or electronic
+          storage is entirely secure.
+        </p>
+        <h2>6. Contact Us</h2>
+        <p>
+          If you have any questions about this Privacy Policy or your personal
+          information, please contact us at Contact us page.{" "}
+          <Link
+            href="/contact-us"
+            className={cn(buttonVariants({ variant: "link" }))}
+          >
+            Contact us
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
--- modules/layout/footer/footer.tsx
+++ modules/layout/footer/footer.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -8,21 +9,27 @@ export default function Footer() {
       <div className="mb-2 flex items-center justify-center gap-4 text-xs">
         <div>
           &copy; {year} Created by{" "}
-          <a
+          <Link
             href="https://github.com/JAHMD"
             target="_blank"
             className="font-semibold text-muted-foreground transition-colors hover:text-white"
           >
             Ahmed Wael
-          </a>
+          </Link>
           .
         </div>
         <Badge variant="secondary">v1.3.0</Badge>
       </div>
-      <p className="text-xs leading-6">
+      <p className="mx-auto max-w-xl text-xs leading-6">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router), TypeScript, Prisma, Tailwind CSS and
-        Vercel hosting.
+        Vercel hosting.{" "}
+        <Link
+          href="/privacy-policy"
+          className="font-semibold transition-colors hover:text-white"
+        >
+          Privacy Policy
+        </Link>
       </p>
     </footer>
   );
